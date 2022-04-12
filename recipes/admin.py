8e225891c19579f6e83f2277e6c649235090e4c9
robindavidgames <robin.david.games@gmail.com>
2022-04-12 10:37:35 +0000

    Add prepopulate slug, filter, list and search to posts in admin panel.

--- recipes/admin.py
+++ recipes/admin.py
@@ -5,4 +5,16 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
+    # Prepopulate the slug field.
+    prepopulated_fields = {'slug': ('title',)}
+
+    # Create admin post filters.
+    list_filter = ('created_on', 'author')
+
+    # Add details to the list display.
+    list_display = ('title', 'slug', 'author', 'created_on')
+
+    # Add a search field.
+    search_fields = ['title', 'content']
+
     summernote_fields = ('content')
