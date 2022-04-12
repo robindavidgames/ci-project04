5c8ba88b78df68bcaaa6cc55acce105878fc1059
robindavidgames <robin.david.games@gmail.com>
2022-04-12 11:51:18 +0000

    Add default templates and views.

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
-# Create your views here.
+class RecipeList(generic.ListView):
+    """
+    Use the Generic Views to create a list of recipes.
+    """
+    model = Recipe
+    # queryset = Recipe.objects.filter(status=1).order_by(-'created_on')
+    queryset = Recipe.objects.filter(status=1)
+    template_name = index.html
+    paginate_by = 6
