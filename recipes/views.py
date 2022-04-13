fe8f1b8ee0ff2bf58fbee89cec3f59f7a409998a
robindavidgames <robin.david.games@gmail.com>
2022-04-13 21:58:04 +0000

    Add allauth.

--- recipes/views.py
+++ recipes/views.py
@@ -20,7 +20,7 @@ class RecipeDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        # comments = Recipe.comments.filter(approved=True).order_by("-created_on")
+        comments = Recipe.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -30,7 +30,7 @@ class RecipeDetail(View):
             'recipe_detail.html',
             {
                 'recipe': recipe,
-                # 'comments': comments,
+                'comments': comments,
                 'liked': liked
             },
         )
