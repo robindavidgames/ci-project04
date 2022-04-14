3c8e917949fb70cc6eb6f0aff179b745d9e3927b
robindavidgames <robin.david.games@gmail.com>
2022-04-14 11:37:41 +0000

    Adding comment feature to recipe pages.

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Recipe
+from .forms import CommentForm
 
 class RecipeList(generic.ListView):
     """
@@ -20,18 +21,19 @@ class RecipeDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        comments = Recipe.comments.filter(approved=True).order_by("-created_on")
+        comments = recipe.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         return render(
             request,
-            'recipe_detail.html',
+            "recipe_detail.html",
             {
-                'recipe': recipe,
-                'comments': comments,
-                'liked': liked
+                "recipe": recipe,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
-
