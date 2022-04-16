48814fb62f5588cd556f9544ab144fa5602b9e02
robindavidgames <robin.david.games@gmail.com>
2022-04-16 10:19:52 +0000

    Fix url path for recipe_likes.

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name="recipe_detail"),
-    path('like/<slug:slug>', views.RecipeLike.as_view(), name="recipe_like"),
+    path('like/<slug:slug>', views.RecipeLike.as_view(), name="recipe_likes"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -73,14 +73,12 @@ class RecipeDetail(View):
 
 
 class RecipeLike(View):
-
-    def post(self, request, slug):
+    
+    def post(self, request, slug, *args, **kwargs):
         recipe = get_object_or_404(Recipe, slug=slug)
-
-        if recipe.likes.filter(id=self.request.user.id).exists():
+        if recipe.likes.filter(id=request.user.id).exists():
             recipe.likes.remove(request.user)
         else:
             recipe.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
-
